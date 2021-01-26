d70898d62ac8142eb786fedab05e0960700a1502
kevmccall <kev_mccall@yahoo.com>
2021-01-26 15:21:23 -0700

    added log mouseenter

--- index.js
+++ index.js
@@ -8,9 +8,42 @@
   let go = document.getElementById("goButton");
   let goBulb = document.getElementById("goLight");
 
-// Event Listener For When Stop Button Is Clicked
+  // Event Listener Toggle For When Stop Button Is Clicked //
   stop.addEventListener("click", function() {
+    // Grabs Red Light In CSS//
     stopBulb.classList.toggle('stop')
   })
+
+  // Event Listener Toggle For When Slow Button Is Clicked //
+  slow.addEventListener("click", function() {
+    // Orange Light //
+    slowBulb.classList.toggle('slow')
+  })
+
+  // Event Listener Toggle For When Go Button Is Clicked //
+  go.addEventListener("click", function() {
+    // Green Light //
+    goBulb.classList.toggle('go')
+  })
+
+
+  // Console Log Mouse Enter //
+  stop.addEventListener('mouseenter', function() {
+    logMouseEnter('stop');
+  });
+
+  slow.addEventListener('mouseenter', function() {
+    logMouseEnter('slow');
+  });
+
+  go.addEventListener('mouseenter', function() {
+    logMouseEnter('go');
+  });
+
+  function logMouseEnter(light) {
+    console.log('Entered' + light + 'button');
+  }
+
+  
 
 })();
