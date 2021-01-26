aa3c3dfaa63d647296463d500ddbf09e98e57f53
kevmccall <kev_mccall@yahoo.com>
2021-01-26 14:44:54 -0700

    initial commit, added variables

---
+++ index.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+// Retrieve Stoplight Elements //
+  let stop = document.getElementById("stopButton");
+  let stopBulb = document.getElementById("stopLight");
+  let slow = document.getElementById("slowButton");
+  let slowBulb = document.getElementById("slowLight");
+  let go = document.getElementById("goButton");
+  let goBulb = document.getElementById("goLight");
+
+})();
